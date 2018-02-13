6eea11cc1f67c3f27c9baa825d001bf403e2fc5f
michalkurka <michalk@h2o.ai>
2018-02-13 10:48:17 -0800

    PUBDEV-5323: GLM doesn't need prediction postprocessing

--- h2o-algos/src/main/java/hex/glm/GLMModel.java
+++ h2o-algos/src/main/java/hex/glm/GLMModel.java
@@ -1115,6 +1115,7 @@ public class GLMModel extends Model<GLMModel,GLMModel.GLMParameters,GLMModel.GLM
     }
     return preds;
   }
+  @Override protected boolean needsPostProcess() { return false; /* pred[0] is already set by score0 */ }
 
   @Override protected void toJavaPredictBody(SBPrintStream body,
                                              CodeGeneratorPipeline classCtx,
